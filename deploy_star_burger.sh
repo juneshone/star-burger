a5e6b7d61818caf9236f4465c4f0f339de95db04
juneshone <shamrova.julia@mail.ru>
2024-09-29 10:11:20 +0300

    Add export of variables for automatic deploy

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -8,6 +8,7 @@ git pull git@github.com:juneshone/star-burger.git
 
 echo Установка библиотек
 source venv/bin/activate
+[ ! -f .env ] || export $(grep -v '^#' .env | xargs)
 pip3 install -r requirements.txt
 npm ci --dev
 
@@ -30,6 +31,6 @@ curl --request POST \
     --url https://api.rollbar.com/api/1/deploy \
     --header 'accept: application/json' \
     --header 'content-type: application/json' \
-    --header 'X-Rollbar-Access-Token: $rollbar_token' \
+    --header 'X-Rollbar-Access-Token: '$rollbar_token'' \
     --data '{"environment": "production", "revision": "'"$hash"'", "rollbar_username": "juneshone", "local_username": "juneshone"}'
 echo Успешное завершение деплоя
